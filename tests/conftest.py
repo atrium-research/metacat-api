876aca5da1db381a34d3fca6a8553fd2591d280b
Kévin Darty <kevin.darty@foxcub.fr>
2026-07-21 11:06:00 +0200

    Fix test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,15 @@
 import pytest
 from fastapi.testclient import TestClient
 
+from metacat_api.config import settings
 from metacat_api.main import app
 
 
 @pytest.fixture
 def client() -> TestClient:
     return TestClient(app)
+
+
+@pytest.fixture(autouse=True)
+def set_settings():
+    settings.json_data_dir = "./example_data"
