a5a10c14f56ab73797b74e20573cc0cc2894b01e
Kévin Darty <kevin.darty@foxcub.fr>
2026-08-10 15:06:21 +0200

    Add human size

--- src/metacat_api/models/backup.py
+++ src/metacat_api/models/backup.py
@@ -1,9 +1,19 @@
-from pydantic import BaseModel
+from functools import cached_property
+
+from pydantic import BaseModel, computed_field
+
+from metacat_api.services.util import sizeof_fmt
 
 
 class DataFile(BaseModel):
     name: str
+    filename: str
     size: int
+
+    @computed_field
+    @cached_property
+    def size_human(self) -> str:
+        return sizeof_fmt(self.size)
 
 
 class BackupInfo(BaseModel):
